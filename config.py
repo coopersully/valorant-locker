896a37425cdd4b34fea71e32d9f613239eab8b78
Cooper Sullivan <thecoopersully@gmail.com>
2023-04-17 18:22:52 -0500

    Remove old environment variables

--- config.py
+++ config.py
@@ -7,9 +7,6 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get("secret_key")
 
-USERS_FILE = os.environ.get("users_path")
-ACCOUNTS_FILE = os.environ.get("accounts_path")
-
 DEFAULT_RANK = "Unknown"
 DEFAULT_RR = "??"
 DEFAULT_LAST_UPDATED = datetime.strptime("01/01/1990", "%m/%d/%Y")
